e874881b237c98b4a475ce65fbb09a5a8f857078
Thomas <thomas@geenbs.nl>
2017-08-13 12:39:56 +0200

    Itegrated sneh header file

--- poc.c
+++ poc.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "snek.h"
+#include "keybInterface.h"
 #define clear() printf("\033[H\033[J") //to clear the linux term
-
-typedef struct snakeSegment{
-    char tok;
-    size_t x;
-    size_t y;
-    size_t d; //Direction
-    struct snakeSegment *next;
-}segm;
-
-typedef struct snake{
-    size_t x;
-    size_t y;
-    size_t d; //Direction
-    segm *seg;
-}snek;
 
 void printField(size_t cols, size_t rows, snek *snek);
 
